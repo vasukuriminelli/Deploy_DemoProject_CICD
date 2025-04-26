9c924217749107a4375bce4758a87434d23db387
vasu <kuriminellivasu@gmail.com>
2025-04-26 23:37:21 +0530

    FirstCommit

---
+++ src/main/java/com/example/demo/Controller/MyController.java
@@ -0,0 +1,15 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+	@GetMapping("/get")
+	public String getData()
+	{
+		return "THIS IS FIRST DEPLOYMED PROJECT THROUGH CICD_CONCEPT";
+	}
+
+}
